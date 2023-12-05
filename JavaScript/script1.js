2d1104433b89093cc598192213ad128da75b81d0
Дмитро <124165148+sd13666@users.noreply.github.com>
2023-12-05 09:00:43 +0200

    added html and css

---
+++ JavaScript/script1.js
@@ -0,0 +1,55 @@
+/* 1. Зробіть функцію, яка приймає параметром число від 1 до 7, 
+а повертає день тижня українською мовою.*/
+
+// -- 1-й варіант -----
+// function numToDay(num) {
+  
+//   switch (num) {
+//     case 1:
+//       console.log("Понеділок");
+//       break;
+//     case 2:
+//       console.log("Вівторок");
+//       break;
+//     case 3:
+//       console.log("Середа");
+//       break;
+//     case 4:
+//       console.log("Четвер");
+//       break;
+//     case 5:
+//       console.log("П'ятниця");
+//       break;
+//     case 6:
+//       console.log("Cубота");
+//       break;
+//     case 7:
+//       console.log("Неділя");
+//       break;
+//     default:
+//       num = 0;
+//     };
+//     return num;
+// };
+
+// numToDay(1);
+// numToDay(2);
+// numToDay(3);
+// numToDay(4);
+// numToDay(5);
+// numToDay(6);
+// numToDay(7);
+// -------------------
+//--- 2-й варіант ------
+// function dayOfWeek(numDay) {
+//   const dayOfWeek = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота", "Неділя"];
+//   if (numDay >= 1 && numDay <= 7) {
+//     return console.log(dayOfWeek[numDay - 1]);
+//   } else {
+//     return console.log("Невірний номер дня");
+//   }
+// }
+
+// dayOfWeek(4);
+// dayOfWeek(8);
+//------------------------
